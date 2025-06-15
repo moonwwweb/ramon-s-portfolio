55f3f507f332b4d2bfdbd1950064d7653fee77be
moonwwweb <ramonhufancia@gmail.com>
2025-06-16 07:39:46 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,11 @@ import Skills from "./Components/Skills";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import GetInTouch from "./Components/GetInTouch";
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+
 
 function App() {
   
-    useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
-  
+
   return (
     <>
       <Header />
